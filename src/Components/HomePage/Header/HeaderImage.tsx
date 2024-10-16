b6247d31a6112ba79cde426b2a7056ee9f480265
Anthony Gonzalez <Gonzalez.anthony0232@gmail.com>
2024-10-16 13:34:45 -0700

    git

--- src/Components/HomePage/Header/HeaderImage.tsx
+++ src/Components/HomePage/Header/HeaderImage.tsx
@@ -1,3 +1,5 @@
+import Soldier from "../../../Soldier.png";
+
 export function HeaderImage() {
-  return <img src="Soldier.png" alt="A soldier" className="relative" />;
+  return <img src={Soldier} alt="A soldier" className="relative" />;
 }
